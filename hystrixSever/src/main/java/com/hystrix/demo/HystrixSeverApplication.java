bc14967cfd4d2e7b8da356c1a183a921c08d5c58
Kumar Abhishek <kumar.abhishek0809@gmail.com>
2018-07-13 19:06:31 +0800

    Hystrix

--- hystrixSever/src/main/java/com/hystrix/demo/HystrixSeverApplication.java
+++ hystrixSever/src/main/java/com/hystrix/demo/HystrixSeverApplication.java
@@ -3,13 +3,18 @@ package com.hystrix.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableHystrixDashboard
+@EnableHystrix
 public class HystrixSeverApplication {
 
 	public static void main(String[] args) {
--- hystrixSever/src/main/java/com/hystrix/demo/controller/SearchFlightController.java
+++ hystrixSever/src/main/java/com/hystrix/demo/controller/SearchFlightController.java
@@ -1,15 +1,21 @@
 package com.hystrix.demo.controller;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.hystrix.demo.model.Flight;
+import com.hystrix.demo.model.Inventory;
 import com.hystrix.demo.model.SearchQuery;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 
 @RestController
 public class SearchFlightController {
@@ -19,10 +25,22 @@ public class SearchFlightController {
 	private RestTemplate restTemplate;
 	
 	@RequestMapping("/searchFlight")
-	public List<Flight> searchFlights(Long delay){
+	@HystrixCommand(fallbackMethod="failOver" , commandProperties = {
+			@HystrixProperty(name="execution.isolation.thread.timoutInMilliseconds",value="500")
+	})
+	public List<Flight> searchFlights(@RequestParam(name="delay",defaultValue="200") Long delay) throws InterruptedException{
+		Thread.sleep(delay);
 		SearchQuery searchQuery = new SearchQuery("NYC", "SFO", "22-JAN-16");
 		Flight[] flights = restTemplate.postForObject(SEARCH_SERVICE + "/get", searchQuery, Flight[].class);
 		return Arrays.asList(flights);
+	}
+	
+	public List<Flight> failOver(Long delay){
+		List<Flight> flights = new ArrayList<Flight>();
+		flights.add(new Flight("KL 987", "IND", "PAK", LocalDate.now().toString(), new com.hystrix.demo.model.Fares("100", "AED"),new Inventory(100)));
+		flights.add(new Flight("KL 987", "IND", "PAK", LocalDate.now().toString(), new com.hystrix.demo.model.Fares("100", "AED"),new Inventory(100)));
+		return flights;
+		
 	}
 
 }

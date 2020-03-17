a2aff2c03ecb1aaa28c80d8067bb908153bd1fb0
KumarAbhishek0809 <kumar.abhishek0809@gmail.com>
2020-03-17 18:10:15 +0400

    clean up

--- zullproxyServer/src/main/java/com/zull/proxy/APIGatewayApplication.java
+++ zullproxyServer/src/main/java/com/zull/proxy/APIGatewayApplication.java
@@ -4,21 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
-
-import com.zull.proxy.filter.CustomZuulFilter;
 
 @SpringBootApplication
 @EnableZuulProxy
-@EnableDiscoveryClient 
-public class Application {
+@EnableDiscoveryClient
+public class APIGatewayApplication {
 	
-	@Bean
-	public CustomZuulFilter proxyRequestHelper() {
-	   return new CustomZuulFilter();
-	}
-
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(APIGatewayApplication.class, args);
 	}
 }

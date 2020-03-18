b7f92f77cd42e945862b90ac4857ff3f7a4d29f5
KumarAbhishek0809 <kumar.abhishek0809@gmail.com>
2020-03-18 13:30:59 +0400

    fixing yaml

--- SpringReactive/src/main/java/com/simple/SpringReactiveApplication.java
+++ SpringReactive/src/main/java/com/simple/SpringReactiveApplication.java
@@ -1,9 +1,8 @@
-	package com.simple.service;
+package com.simple;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -11,10 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableDiscoveryClient
 public class SpringReactiveApplication {
 	
-	@RequestMapping("/execute")
-	public String execute() {
-		return "Executed";
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringReactiveApplication.class, args);

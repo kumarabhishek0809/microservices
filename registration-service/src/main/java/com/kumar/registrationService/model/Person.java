f49c37902f84e632e7337dba3e418412eefaca0f
KumarAbhishek0809 <kumar.abhishek0809@gmail.com>
2020-04-28 14:50:29 +0400

    rabbit-mq

--- registration-service/src/main/java/com/kumar/registrationService/model/Person.java
+++ registration-service/src/main/java/com/kumar/registrationService/model/Person.java
@@ -2,9 +2,22 @@ package com.kumar.registrationService.model;
 
 import java.io.Serializable;
 
+
 public class Person implements Serializable {
+
+    private static final long serialVersionUID = 4L;
+
     private int id;
     private String name;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
 
     public Person(int id, String name) {
         this.id = id;

db941195e3402ebe7ea8bb9c479e34fc8f5bd3d2
Kumar Abhishek <mail2kumar.2021@gmail.com>
2020-12-31 18:07:52 +0400

    test

--- spring-resilience4j/src/main/java/com/simple/SpringResilience.java
+++ spring-resilience4j/src/main/java/com/simple/SpringResilience.java
@@ -1,17 +1,18 @@
 package com.simple;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 @EnableDiscoveryClient
-public class Application {
+public class SpringResilience {
 
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		new SpringApplicationBuilder(SpringResilience.class).web(WebApplicationType.SERVLET).run(args);
 	}
 }

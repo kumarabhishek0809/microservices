1b0d3b541b7db4ea171202c685865c5fb021a2c4
Kumar <mail2kumar.2021@gmail.com>
2021-07-21 16:50:22 +0400

    21JULY2021

--- registration-service/src/main/java/com/kumar/registrationService/cqrs/exception/SellerServiceEventsErrorHandler.java
+++ registration-service/src/main/java/com/kumar/registrationService/cqrs/exception/SellerServiceEventsErrorHandler.java
@@ -1,5 +1,15 @@
 package com.kumar.registrationService.cqrs.exception;
 
-public class SellerServiceEventsErrorHandler {
+import org.axonframework.eventhandling.EventMessage;
+import org.axonframework.eventhandling.EventMessageHandler;
+import org.axonframework.eventhandling.ListenerInvocationErrorHandler;
+
+public class SellerServiceEventsErrorHandler implements ListenerInvocationErrorHandler {
+
+	@Override
+	public void onError(Exception exception, EventMessage<?> event, EventMessageHandler eventHandler) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- search-flight-tickets/README.md
+++
@@ -1,5 +0,0 @@
-# SearchFlightTickets
-SearchFlightTickets
-
-
-Server.port had been set in eclipse externally that is why it is not in ConfigurationProperties.
